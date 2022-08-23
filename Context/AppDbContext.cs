35b8ca751d085d7fe722cc5a705e24af948ca0ba
Emmanuel Cuevas <emmanuelcuevasm1@gmail.com>
2022-08-23 11:44:03 -0400

    init

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealState.Dtos;
+
+namespace RealState.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _configuration;
+        public AppDbContext(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=67.205.146.248;port=5432;Database=qa;User Id=postgres;Password=Delgado123;searchPath=dbo");
+            }
+        }
+
+        public virtual DbSet<Step> Steps { get; set; }
+    }
+}
---
+++ Dtos/Step.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealState.Dtos
+{
+    public class Step
+    {
+        public string fact { get; set; }
+        public int length { get; set; }
+    }
+}
